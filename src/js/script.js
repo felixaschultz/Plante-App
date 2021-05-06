da1e7f3077f5a246df896b3dd039dcb3f5a1a6ea
intastellar <70743293+intastellar@users.noreply.github.com>
2021-05-06 12:39:59 +0200

    Fik API til at køre

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,22 @@
-function sendIdentification() {
-    const files = [document.querySelector(".profileHome__image").src];
+function sendIdentification(file) {
+    const files = [file];
     const promises = files.map((file) => {
       return new Promise((resolve, reject) => {
           const reader = new FileReader();
           reader.onload = (event) => {
             const res = event.target.result;
-            console.log(res);
             resolve(res);
           }
-          file = URL.createObjectURL(file);
-          console.log(file);
-
-          reader.readAsDataURL(file)
+          reader.readAsDataURL(file);
       })
     })
     
     Promise.all(promises).then((base64files) => {
             
+      console.log(base64files);
       const data = {
         api_key: "iF01Etqb3LEywM5G0TCH4ikp8MZPTH3kLm5jQs7Hs6DSOXjdZV",
-        images: document.querySelector(".profileHome__image").src,
+        images: base64files,
         modifiers: ["crops_fast", "similar_images"],
         plant_language: "en",
         plant_details: ["common_names",
@@ -40,21 +37,38 @@ function sendIdentification() {
       .then(response => response.json())
       .then(data => {
         console.log('Success:', data);
+        const PlantName = data.suggestions[0].plant_details.scientific_name;
+        const des = data.suggestions[0].plant_details.wiki_description.value;
         const body = document.querySelector(".profileHome__Info--text");
+        const hadLine = document.querySelector(".profileHome__name");
+        const profileDes = document.querySelector(".profileHome__description");
 
-
+        hadLine.textContent = PlantName;
+        profileDes.textContent = des;
+        /* body.innerHTML = `<h2>Science Name: ${PlantName}</h2>
+        <p>${des}</p>`; */
       })
       .catch((error) => {
         console.error('Error:', error);
       });
     })
-  
 };
 
 const messages = document.querySelector(".messages");
 const loadingScreen = document.querySelector(".loadingScreen");
 const profile = document.querySelector(".profileHome");
 const messageBTN = document.querySelector(".--messNav");
+const image = document.querySelector(".profileHome__image");
+const tro = document.querySelector(".--click");
+
+tro.addEventListener("click", function(){
+  document.querySelector(".trophy-Big").classList.toggle("--big");
+  setTimeout(function(){
+    if(document.querySelector(".trophy-Big").classList.contains("--big")){
+      document.querySelector(".trophy-Big").classList.toggle("--big");
+    }
+  },1000);
+});
 
 function loadingScreenLoading(){
     if(window.location.href.indexOf("app/index.html") > -1){
@@ -64,13 +78,27 @@ function loadingScreenLoading(){
     }
 }
 
-profile.addEventListener("click", function(){
+profile.addEventListener("click", function(e){
+    
     this.classList.toggle("--expand");
-    sendIdentification();
+    if(this.classList.contains("--expand")){
+      fetchImage(image.src);
+    }
 })
+
+fetchImage(image.src);
 
 messageBTN.addEventListener("click", function(){
     messages.classList.toggle("--expand"); 
 })
+
+function fetchImage(url){
+  fetch(url).then(response => {
+    return response.blob();
+  }).then(file => {
+      console.log(file);
+      sendIdentification(file);
+  })
+}
 
 loadingScreenLoading();
